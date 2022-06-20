b1533e8096f8117b75033bb5e7fe9a5ad6c42dcf
calvinkenn <calvink.palma@gmail.com>
2022-06-20 23:42:29 +0800

    setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MainNavigation />;
 }
 
 export default App;
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MainNavigation = () => {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid d-flex justify-content-center">
+        <a class="navbar-brand active" href="#">
+          Projects
+        </a>
+        <a class="navbar-brand" href="#">
+          Certificates
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default MainNavigation;
